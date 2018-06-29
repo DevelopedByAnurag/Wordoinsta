7edcaf47618f368c96e4718a497216928979f4dc
Anurag Batra <25814581+DevelopedByAnurag@users.noreply.github.com>
2018-06-29 14:29:07 +0530

    Update Wordoinsta.sh

--- Wordoinsta.sh
+++ Wordoinsta.sh
@@ -34,46 +34,47 @@ if [ "$setupinfo" == 1 ] ; then
 	sudo apt-get install php php-mysql php-fpm php-curl php-gd php-pear php-imagick php-imap php-mcrypt php-recode php-tidy php-xmlrpc
 	sudo nginx -t
 	sudo systemctl restart nginx
+fi
+echo "Database Name: "
+read -e dbname
+echo "Database User: "
+read -e dbuser
+echo "Database Password: "
+read -s dbpass
+echo "Sure run install? (y/n)"
+read -e run
+if [ "$run" == n ] ; then
+	exit
 else
-	echo "Database Name: "
-	read -e dbname
-	echo "Database User: "
-	read -e dbuser
-	echo "Database Password: "
-	read -s dbpass
-	echo "Sure run install? (y/n)"
-	read -e run
-	if [ "$run" == n ] ; then
-		exit
-	else
-		echo "============================================"
-		echo "A robot is now installing WordPress for you."
-		echo "============================================"
-		curl -O https://wordpress.org/latest.tar.gz
-		tar -zxvf latest.tar.gz
-		cd wordpress
-		cp -rf . ..
-		cd ..
-		rm -R wordpress
-		cp wp-config-sample.php wp-config.php
-		perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
-		perl -pi -e "s/username_here/$dbuser/g" wp-config.php
-		perl -pi -e "s/password_here/$dbpass/g" wp-config.php
-		perl -i -pe'
-		  BEGIN {
-		    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-    		push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
-  			  sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
-  			}
- 		 s/put your unique phrase here/salt()/ge
-		' wp-config.php
-		mkdir wp-content/uploads
-		chmod 775 wp-content/uploads
-		echo "Cleaning... JUNK FIES"
-		rm latest.tar.gz
-		rm Wordoinsta.sh
-		echo "========================="
-		echo "Installation is complete."
-		echo "========================="
-	fi
+	echo "============================================"
+	echo "A robot is now installing WordPress for you."
+	echo "============================================"
+	curl -O https://wordpress.org/latest.tar.gz
+	tar -zxvf latest.tar.gz
+	cd wordpress
+	cp -rf . ..
+	cd ..
+	rm -R wordpress
+	cp wp-config-sample.php wp-config.php
+	perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+	perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+	perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+
+	perl -i -pe'
+	  BEGIN {
+	    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+	    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+	    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+	  }
+	  s/put your unique phrase here/salt()/ge
+	' wp-config.php
+
+	mkdir wp-content/uploads
+	chmod 775 wp-content/uploads
+	echo "Cleaning..."
+	rm latest.tar.gz
+	rm Wordoinsta.sh
+	echo "========================="
+	echo "Installation is complete."
+	echo "========================="
 fi
